31526b41cfa9600d4ea607b6be990ac4a24d75cd
Dhurga <dhurgadharani3@gmail.com>
2024-12-24 15:12:21 +0800

    Comments added

--- to-do-list.c
+++ to-do-list.c
@@ -5,12 +5,12 @@
 
 void view_tasks(char*** list, int* size)
 {
-    int list_length = *size;
+    int list_length = *size; //The size is the list_length
 
     printf("\n");
     for (int i=0; i<list_length; i++)
     {
-        printf("Task %d: %s\n", i+1, (*list)[i]);
+        printf("Task %d: %s\n", i+1, (*list)[i]); //iterates through the list and prints all the tasks
     }
     printf("\n");
 }
@@ -30,17 +30,17 @@ int add_task(char*** list, int* size) //* is an array of strings, ** is a pointe
 
     printf("Enter your task here:\n");
 
-    char temp_task[100]; 
-    scanf("%s", temp_task);
+    char temp_task[100]; //Gives the user 100 characters to enter thei task name
+    scanf("%s", temp_task); //Saves the input to temp_task
 
 
-    (*list)[(*size) - 1] = (char*)malloc((strlen(temp_task)+1) * sizeof(char));
+    (*list)[(*size) - 1] = (char*)malloc((strlen(temp_task)+1) * sizeof(char)); //In the last index of the array, allocate enough char memory for the task
 
-    strcpy(((*list)[(*size)-1]), temp_task);
+    strcpy(((*list)[(*size)-1]), temp_task); //Copy the task into the array
 
-    view_tasks(list, size);
+    view_tasks(list, size); 
 
-    return 0;
+    return 0; //Everything is okay
 
 }
 
@@ -52,21 +52,19 @@ int delete_task(char*** list, int* size)
     int index;
     scanf("%d", &index);
 
-    if ((*size)<=0)
+    if ((*size)<=0) //If there is more than 0 tasks
     {
-        if(index < 0 || index > (*size))
+        if(index <= 0 || index > (*size)) //And the task number is 0 or greater than the list size+1 (index shown to user is +1)
         {
             printf("\nInvalid task number.\n");
-            return 1;
+            return 1; //Returns to main() if there is an error
         }
         printf("\nNo tasks in the list.\n");
-        return 2;
+        return 2; //Returns to main() if there is an error
     }
     
 
-    printf("\nTask deleted: Task%d: %s\n", index, (*list)[index-1]);
-
-    char** temp_list = (char**)malloc(((*size)-1)*(sizeof(char*)));
+    printf("\nTask deleted: Task%d: %s\n", index, (*list)[index-1]); //Tell the user the task being deleted
 
     //Move all the elements in the array up one so the task the user wants to delete is gone,
     //And all the other elements move up the list, filling up the gap.
@@ -75,12 +73,12 @@ int delete_task(char*** list, int* size)
         (*list)[i] = (*list)[i+1];
     }
 
-    (*list)[(*size)] = 0 ;
-    ((*size)--);
+    (*list)[(*size)] = 0 ; //Assign the last index in the list 0
+    ((*size)--); //Reduce the size of the list by 1
 
     view_tasks(list, size);
 
-    return 0;
+    return 0; //Everything is okay
 
 }
 
@@ -119,10 +117,10 @@ bool starting_screen(bool* exit_p_now, char*** list, int* size)
 int main()
 {
     bool exit_program = false;
-    bool* exitP = &exit_program;
+    bool* exitP = &exit_program; //Pointer for exit program
 
-    int size = 0;
-    char** tasks = NULL;
+    int size = 0; //Beginning size of array
+    char** tasks = NULL; //Array is a list of pointers pointing to a memory location of array elements
     
     
     while (exit_program == false)
@@ -134,10 +132,10 @@ int main()
 
     for(int i=0; i<(size); i++)
     {
-        free((tasks)[i]);
+        free((tasks)[i]); //Free each element space in the array
     }
 
-    free(tasks);
+    free(tasks); //Free the array itself
     printf("Program exited.\n");
 
 }
